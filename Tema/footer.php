78abeda944462acdf9919de153218300ddfadce3
Paulo <pauloh16.pdr@gmail.com>
2020-01-17 10:30:40 -0300

    Adicionar custom fields no footer

--- Tema/footer.php
+++ Tema/footer.php
@@ -1,3 +1,4 @@
+<?php 	$contato = get_page_by_title('contato'); ?>
 <footer>
         <div class="container">
             <a href="/" class="grid-3">
@@ -6,13 +7,13 @@
             <div class="grid-4">
                 <h3>Redes sociais</h3>
                 <div class="links_sociais">
-                    <a>
+                <a href="<?php the_field('link_facebook', $contato); ?>">
                         <img src="<?php echo get_template_directory_uri(); ?>/img/Ícones/facebook_icon.svg" alt="ícone Facebook">
                     </a>
-                    <a>
+                    <a href="<?php the_field('link_instagram', $contato); ?>">
                         <img src="<?php echo get_template_directory_uri(); ?>/img/Ícones/instagram_icon.svg" alt="ícone Facebook">
                     </a>
-                    <a>
+                    <a href="<?php the_field('link_twitter', $contato); ?>">
                         <img src="<?php echo get_template_directory_uri(); ?>/img/Ícones/twitter_icon.svg" alt="ícone Facebook">
                     </a>
                 </div>
@@ -20,10 +21,10 @@
             <div class="grid-6">
                 <h3>Contato</h3>
                 <ul>
-                    <li>- Av. Primavera, 107 | Vargem Grande</li>
-                    <li>- São Paulo, SP</li>
-                    <li>- rita_seilaoq@gmail.com</li>
-                    <li>- (11) 99999-9999</li>
+                    <li>- <?php the_field('rua', $contato); ?> | <?php the_field('bairro', $contato); ?></li>
+                    <li>- <?php the_field('cidade_estado', $contato); ?></li>
+                    <li>- <?php the_field('email', $contato); ?></li>
+                    <li>- <?php the_field('telefone', $contato); ?></li>
                 </ul>
             </div>
         </div>
