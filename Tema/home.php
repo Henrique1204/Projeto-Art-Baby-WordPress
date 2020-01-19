88d902c6a6b73b471ecc43bdcd851045e2c8da61
Paulo <pauloh16.pdr@gmail.com>
2020-01-19 17:41:56 -0300

    Ajustar responsividade da página blog e de posts

--- Tema/home.php
+++ Tema/home.php
@@ -4,21 +4,29 @@
 <section class="introducao_blog">
     <h1 class="titulo">Blog</h1>
 </section>
-<section class="posts_blog">
+<section class="posts_blog alinhar_centro">
     <div class="container alinhar_centro">
         <?php if(have_posts()): while(have_posts()): the_post(); ?>
             <article class="grid-12 blog_post">
                 <h3 class="data_post"><?php echo get_the_time('d/m/Y'); ?></h3>
-                <div>
+                <div class="post_desktop">
                     <img src="<?php the_field('thumbnail_post'); ?>" alt="thumbnail do post">
                     <div class="conteudo_post">
                         <h2 class="sub_titulo"><?php the_title(); ?></h2>
                         <p><?php the_field('resumo_post'); ?></p>
                     </div> 
-                    <a href="<?php the_permalink(); ?>" class="btn_blog alinhar_centro"></a>
+                    <a href="<?php the_permalink(); ?>" class="btn_blog"></a>
                 </div>
+                <a href="<?php the_permalink(); ?>" class="post_smartphone"><?php the_title(); ?></a>
             </article>
-        <?php endwhile; endif; ?>
+        <?php endwhile; ?>
+        <div class="nav_blog grid-12">
+		    <?php
+                previous_posts_link('<< Anterior');
+			    next_posts_link('Próximo >>');
+		    ?>
+	    </div>
+        <?php endif; ?>
     </div>
 </section>
 
--- Tema/index.php
+++ Tema/index.php
@@ -1,4 +1,9 @@
 <?php get_header(); ?>
+<?php
+    $imagem_id = get_field('img_post');
+    $background_large = wp_get_attachment_image_src($imagem_id, 'large');
+    $background_medium = wp_get_attachment_image_src($imagem_id, 'medium');
+?>
 <?php if(have_posts()): while(have_posts()): the_post(); ?>
     <article>
         <section  class="introducao_blog intro_geral">
@@ -7,11 +12,6 @@
             </div>
         </section>
         <section class="container conteudo_geral">
-            <?php
-		        $imagem_id = get_field('img_post');
-		        $background_large = wp_get_attachment_image_src($imagem_id, 'large');
-                $background_medium = wp_get_attachment_image_src($imagem_id, 'medium');
-    	    ?>
             <div>
                 <img src="<?php echo $background_large[0]; ?>" alt="<?php the_field('alt_img_post') ?>" class="grid-10 img_post">
                 <img src="<?php echo $background_medium[0]; ?>" alt="<?php the_field('alt_img_post') ?>" class="grid-10 img_post">
@@ -19,11 +19,11 @@
                     <p><?php the_field('texto_principal'); ?></p>
                 </div>
             </div>
-            <div class="grid-16 texto_post_opcional">
                 <?php if(have_rows('textos_pots')): while(have_rows('textos_pots')): the_row(); ?>
-                    <p><?php the_sub_field('texto_secundario'); ?></p>
+                    <div class="grid-16 texto_post_opcional">
+                        <p><?php the_sub_field('texto_secundario'); ?></p>
+                    </div>
                 <?php endwhile; endif; ?>
-            </div>
             <div>
                 <h3 class="data_post"><?php echo get_the_time('d/m/Y'); ?></h3>
                 <a href="http://art-baby.local/blog/" class="btn_voltar">Posts</a>
